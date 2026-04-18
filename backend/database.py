f30bc2a595baa944e03521e4c3c4c958708bdcf4
Ugandhar Telaganeni <nitya050905@gmail.com>
2026-04-18 17:50:13 +0530

    update database.py to use render postgres

--- backend/database.py
+++ backend/database.py
@@ -2,16 +2,18 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
 
-DB_CONFIG = {
-    "host": "localhost",
-    "database": "edgeforge",
-    "user": "postgres",
-    "password": "postgres",
-    "port": 5432
-}
-
 def get_connection():
-    return psycopg2.connect(**DB_CONFIG, cursor_factory=RealDictCursor)
+    db_url = os.environ.get("DATABASE_URL")
+    if db_url:
+        return psycopg2.connect(db_url, cursor_factory=RealDictCursor)
+    return psycopg2.connect(
+        host="localhost",
+        database="edgeforge",
+        user="postgres",
+        password="postgres",
+        port=5432,
+        cursor_factory=RealDictCursor
+    )
 
 def init_db():
     conn = get_connection()
